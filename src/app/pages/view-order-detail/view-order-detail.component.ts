21411bccb37ca5eb72fc2610fedbba9acebcc2d8
Sebastian Obando Paniagua <60985278+sebas2002cr@users.noreply.github.com>
2022-06-14 22:11:25 -0600

    view order details

---
+++ src/app/pages/view-order-detail/view-order-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-order-detail',
+  templateUrl: './view-order-detail.component.html',
+  styleUrls: ['./view-order-detail.component.scss']
+})
+export class ViewOrderDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
